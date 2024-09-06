ea52da33cb504a2bc5a5585bcb83c88dca8b11c1
lukasjanusauskas <lukasjanusauskaslj@gmail.com>
2024-09-06 12:24:15 +0300

    Pridėtas vidurkio, medianos ir galutinio balo skaičiavimas.

--- main.cpp
+++ main.cpp
@@ -1,18 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
-struct Studentas;
-Studentas irasyti_studenta();
-std::vector<Studentas> irasyti_studentus();
-
-int main() {
-	std::setlocale(LC_ALL, "Lithuanian");
-
-	irasyti_studentus();
-
-	return 0;
-}
+#include <algorithm>
 
 struct Studentas {
 	std::string vardas;
@@ -20,6 +9,22 @@ struct Studentas {
 	std::vector<int> nd_pazymiai;
 	int egz_pazymys{};
 };
+
+Studentas irasyti_studenta();
+float vidurkis(std::vector<int> pazymiai);
+float mediana(std::vector<int> pazymiai);
+float galutinis(float paz_agg, int egz_paz);
+std::vector<Studentas> irasyti_studentus();
+
+int main() {
+	std::setlocale(LC_ALL, "Lithuanian");
+
+	std::vector<Studentas> stud = irasyti_studentus();
+	for (auto s : stud)
+		std::cout << mediana(s.nd_pazymiai) << " " << vidurkis(s.nd_pazymiai) << std::endl;
+
+	return 0;
+}
 
 Studentas irasyti_studenta() {
 	Studentas stud;
@@ -56,12 +61,38 @@ std::vector<Studentas> irasyti_studentus() {
 		{
 		case 't':
 			sarasas.push_back(irasyti_studenta());
-			continue;
+			break;
 		case 'n':
 			goto break_loop;
 		default:
-			continue;
+			break;
 		}
 	}
 break_loop: return sarasas;
+}
+
+float vidurkis(std::vector<int> pazymiai) {
+	int sum = 0;
+	for (int paz : pazymiai)
+		sum += paz;
+
+	return ((float)sum) / (pazymiai.size());
+}
+
+float mediana(std::vector<int> pazymiai) {
+	int ilgis = pazymiai.size();
+	if (ilgis == 0)
+		return 0.0;
+
+	std::sort(pazymiai.begin(), pazymiai.end());
+
+
+	if (ilgis % 2 == 1)
+		return pazymiai.at(ilgis / 2);
+	else
+		return (pazymiai.at(ilgis / 2) + pazymiai.at(ilgis / 2 - 1)) / 2.0;
+}
+
+float galutinis(float paz_agg, int egz_paz) {
+	return 0.4 * paz_agg + 0.6 * egz_paz;
 }

d04dd8ff352c3d0cd606f22c38a721816d12f89e
lukasjanusauskas <lukasjanusauskaslj@gmail.com>
2024-09-09 05:08:29 +0300

    Funkcijų ir struktūrų aprašai sukelti į header failus.

---
+++ studentas.h
@@ -0,0 +1,19 @@
+#ifndef STUDENTAS_H
+#define STUDENTAS_H
+
+#include <string>
+#include <vector>
+
+struct Studentas {
+	std::string vardas;
+	std::string pavarde;
+	std::vector<int> nd_pazymiai;
+	int egz_pazymys{};
+};
+
+Studentas irasyti_studenta();
+std::vector<Studentas> irasyti_studentus();
+void spausdinti_stud_duom(Studentas stud);
+void spausdinti_rezultatus(std::vector<Studentas> stud);
+
+#endif
